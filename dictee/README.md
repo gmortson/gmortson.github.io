a820016168bc91a1f801d35fbd428ffa686c02cd
gmortson <gmortson@esri.ca>
2016-02-13 11:11:32 -0500

    updated site to trim whitespace and cleaned up code

---
+++ dictee/README.md
@@ -0,0 +1,13 @@
+# Dictee Website of Destiny
+
+A site that kids can use to help them with their dictee words each week.
+
+## Usage
+
+Enter a dictee word and hit enter. 
+
+You'll have to include the accents as well. To do this enable the french keyboard and hold down the letter to access the accented character.
+
+## History
+
+Updated for the week of Feb 8th
--- dictee/main.js
+++ dictee/main.js
@@ -1,23 +1,13 @@
-function myFunction() {
+function checkValue() {
 	var array = ["le ballon", "le carton", "un camion", "rond", "un ongle"]
-	var value = document.getElementById("dictee").value;
-	var lvalue = value.toLowerCase();
-	console.log(lvalue);
-	isInArray(lvalue, array);
+	var value = document.getElementById("dictee").value.trim().toLowerCase();
+	isInArray(value, array);
 }
 function isInArray(value, array) {
-	console.log(array.indexOf(value));
   	if (array.indexOf(value) > -1) {
  		$('#rightModal').modal('show');
- 		//Array of quotes
-    	var randomImages= new Array ("giphy.gif", "giphy1.gif", "giphy2.gif", "giphy3.gif");
-
-    	//Random number to select an image. Use 8, not 7, since javascript arrays are 0 based
-    	//Math.floor will take us from a floating point number to an integer base
+    	var randomImages= new Array ("giphy.gif", "giphy1.gif", "giphy2.gif", "giphy3.gif", "");
     	var randomNumber = Math.floor((randomImages.length-1)*Math.random());
-    	console.log(randomNumber);
-
-    	//Display that image
     	$('#correctImg').prop('src',randomImages[randomNumber]);
   	} else {
   		$('#tryAgainModal').modal('show');
